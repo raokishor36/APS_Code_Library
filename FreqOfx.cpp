f23ec2e6972a87ba24bc375e1ebc9a5514fec094
Kishor R Rao <43129439+raokishor36@users.noreply.github.com>
2021-06-14 22:19:26 +0530

    Create FreqOfx.cpp

---
+++ FreqOfx.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int countOccurrences(int arr[], int n, int x)
+{
+	int res = 0;
+	for (int i=0; i<n; i++)
+		if (x == arr[i])
+		res++;
+	return res;
+}
