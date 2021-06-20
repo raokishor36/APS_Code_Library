162496771c421ac5b188acc0337c143768e7ac77
Kishor R Rao <43129439+raokishor36@users.noreply.github.com>
2021-06-21 00:49:36 +0530

    Create HCFofArray.cpp

---
+++ HCFofArray.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b)
+{
+	if (a == 0)
+		return b;
+	return gcd(b % a, a);
+}
+
+int findGCD(int arr[], int n)
+{
+	int result = arr[0];
+	for (int i = 1; i < n; i++)
+	{
+		result = gcd(arr[i], result);
+
+		if(result == 1)
+		{
+		return 1;
+		}
+	}
+	return result;
+}
