4f011d92f45885c84070bce35a9cf6aed3c5a82e
Kishor R Rao <43129439+raokishor36@users.noreply.github.com>
2021-06-21 01:02:15 +0530

    Create BinomialCoefficient.cpp

---
+++ BinomialCoefficient.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binomialCoeff(int n, int k)
+{
+	if (k > n)
+		return 0;
+	if (k == 0 || k == n)
+		return 1;
+	return binomialCoeff(n - 1, k - 1)
+		+ binomialCoeff(n - 1, k);
+}
