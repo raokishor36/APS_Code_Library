64d17d19563e6793cedda478e908ecf64055c40f
Kishor R Rao <43129439+raokishor36@users.noreply.github.com>
2021-06-21 00:25:08 +0530

    Create MaximumAreaQuadrilateral.cpp

---
+++ MaximumAreaQuadrilateral.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double maxArea(double a, double b, double c, double d)
+{
+	double semiperimeter = (a + b + c + d) / 2;
+	return sqrt((semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c) * (semiperimeter - d));
+}
